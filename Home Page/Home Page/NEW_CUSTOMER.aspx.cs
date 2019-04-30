9f577c5071d19b4c5d26063f4cbb8a6c52dd73e8
Irwin Dominguez-Romero <irwin@ou.edu>
2019-04-29 20:41:42 -0500

    cs

--- Home Page/Home Page/NEW_CUSTOMER.aspx.cs
+++ Home Page/Home Page/NEW_CUSTOMER.aspx.cs
@@ -14,7 +14,7 @@ namespace Home_Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Customer cust = new Customer(); 
+        
             
         }
 
@@ -24,117 +24,4 @@ namespace Home_Page
         }
     }
 
-    public class Customer
-    {
-        public int CustomerID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int ZipCode { get; set; }
-
-    }
-
-    public Create(string firstName, string lastName, string street, string city, string state, int zipcode)
-    {
-      
-        Customer cust = new Customer();
-        cust.FirstName = firstName;
-        cust.LastName = lastName;
-        cust.Street = street;
-        cust.City = city;
-        cust.State = state;
-        cust.ZipCode = zipcode;
-
-        Customer.AddCustomer(cust);
-
-
-     
-    }
-
-    public void AddCustomer(Customer cust)
-    {
-        String connString = ConfigurationManager.ConnectionStrings["CustomerCon"].ConnectionString;
-        using (SqlConnection sqlCon = new SqlConnection(connString))
-        {
-            SqlCommand command = new SqlCommand("spAddCustomer", sqlCon);
-            command.CommandType = CommandType.StoredProcedure;
-
-            SqlParameter paramFirstName = new SqlParameter
-            {
-                ParameterName = "@FirstName",
-                Value = cust.FirstName
-            };
-            command.Parameters.Add(paramFirstName);
-
-            SqlParameter paramLastName = new SqlParameter
-            {
-                ParameterName = "@LastName",
-                Value = cust.FirstName
-            };
-            command.Parameters.Add(paramLastName);
-
-            SqlParameter paramStreet = new SqlParameter
-            {
-                ParameterName = "@Street",
-                Value = cust.Street
-            };
-            command.Parameters.Add(paramStreet);
-
-            SqlParameter paramCity = new SqlParameter
-            {
-                ParameterName = "@City",
-                Value = cust.City
-            };
-            command.Parameters.Add(paramCity);
-
-            SqlParameter paramState = new SqlParameter
-            {
-                ParameterName = "@State",
-                Value = cust.State
-            };
-            command.Parameters.Add(paramState);
-
-            SqlParameter paramZipCode = new SqlParameter
-            {
-                ParameterName = "@ZipCode",
-                Value = cust.ZipCode
-            };
-            command.Parameters.Add(paramZipCode);
-
-            sqlCon.Open();
-            command.ExecuteNonQuery();
-        }
-    }
-
-    public IEnumerable<Customer> Customers
-    {
-        get
-        {
-            String connString = ConfigurationManager.ConnectionStrings["CustomerCon"].ConnectionString;
-            List<Customer> custy = new List<Customer>();
-            using (SqlConnection con = new SqlConnection(connString))
-            {
-                SqlCommand cmd = new SqlCommand("Select * From Customer", con);
-                cmd.CommandType = CommandType.Text;
-                con.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Customer customer = new Customer();
-                    customer.CustomerID = Convert.ToInt32(reader[0]);
-                    customer.FirstName = reader[1].ToString();
-                    customer.LastName = reader[2].ToString();
-                    customer.Street = reader[3].ToString();
-                    customer.City = reader[4].ToString();
-                    customer.State = reader[5].ToString();
-                    customer.ZipCode = Convert.ToInt32(reader[6]);
-
-                    custy.Add(customer);
-                }
-                return custy;
-            }
-        }
-    }
 }

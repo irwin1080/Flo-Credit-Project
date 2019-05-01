2a0c6d054c5935e339c443ba22ed4003c93c8e30
Katharine Pancerz <kpancerz@ou.edu>
2019-04-30 20:23:34 -0500

    Login_Page has changes to connect to database- does nothing

--- Home Page/Home Page/LOGIN_PAGE.aspx.cs
+++ Home Page/Home Page/LOGIN_PAGE.aspx.cs
@@ -16,8 +16,34 @@ namespace Home_Page
         {
             
         }
-           
 
-  
+
+        protected void btnButton1_Click(object sender, EventArgs e)
+        {
+            string userID, password;
+            userID = txtUserID.Text;
+            password = txtPassword.Text;
+
+            string connStr = ConfigurationManager.ConnectionStrings["CustomerCon"].ConnectionString;
+            OleDbConnection connection = new OleDbConnection(connStr);
+            //SqlConnection connectionString = new SqlConnection(@"Data Source=customer-flo-credit.database.windows.net;Initial Catalog=CUSTOMER_DB;User ID=Customer;Password=FloCredit1;Connect Timeout=60;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //SqlCommand cmd = new SqlCommand("spEditCustomer", connectionString);
+            connection.Open();
+
+            OleDbCommand com = new OleDbCommand("SELECT * FROM Customer WHERE [UserName]='" + userID + " 'AND [Password]='" + password
+                + "'", connection);
+
+            OleDbDataReader reader = com.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Response.Redirect("EXISTING_USER.aspx");
+            }
+            else
+            {
+                Response.Redirect("NEW_CUSTOMER.aspx");
+            }
+        }
+
+
     }
 }
